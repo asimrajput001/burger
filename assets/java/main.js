1a277d6192209b81d9b773d6d590ea18390b0d30
asimrajput001 <99793547+asimrajput001@users.noreply.github.com>
2022-02-16 13:24:57 +0500

    initial commit

---
+++ assets/java/main.js
@@ -0,0 +1,12 @@
+function toggleShow(){
+    var main_menu = document.getElementById('main_menu');
+    var btn_S =  document.getElementById(btn_S);
+    main_menu.style.right = '0';
+
+}
+function toggleHide(){
+    var main_menu = document.getElementById('main_menu');
+    var btn_H =  document.getElementById(btn_H);
+    main_menu.style.right = '-200px';
+
+}
